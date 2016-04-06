111e2a0a8b2d4aa5da1aa592ccfa3688594eb935
ajasif2 <ajasif@gmail.com>
2016-04-05 18:56:38 -0700

    Testing Commit
    Added private method which does nothing.

--- src/Placeholder.java
+++ src/Placeholder.java
@@ -2,10 +2,19 @@
  * Write some shit here!!!
  */
 public class Placeholder {
+	
+	private String temp;
+	
 	public Placeholder(){
 		//TODO: finish constructor
 		//Rascal
 		//Hello!
+		temp = test();
+		System.out.println(temp);
 	}
 	
+	private String test() {
+		String fail = "I do nothing";
+		return fail;
+	}
 }
