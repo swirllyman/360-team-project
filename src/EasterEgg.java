e94745989fdc50b684ee8a8ebdcbf35a5f059e29
ajasif2 <ajasif@gmail.com>
2016-05-07 20:56:45 -0700

    Created Easter Egg
    Initialized Easter Egg behavior.

---
+++ src/EasterEgg.java
@@ -0,0 +1,55 @@
+/*
+ * Easter Egg Iteration
+ * Description: Program outputs a description of team TryHard.
+ */
+
+/**
+ * 
+ * @author Asif Jamal
+ * @author Dhruv Balakrishnan
+ * @author Chinh Nguyen
+ * @author Biirhanu Zerefa
+ */
+public class EasterEgg {
+	
+	public EasterEgg(){
+		
+	}
+	
+	public void run() {
+		System.out.println("We are team TryHard.");
+	}
+	
+	/**
+	 * Post: Outputs a description of the first member
+	 * of team TryHard.
+	 */
+	public void personOne() {
+		System.out.println("Biirhanu is a member of team TryHard.");
+	}
+	
+	/**
+	 * Post: Outputs a description of the second member 
+	 * of team TryHard.
+	 */
+	public void personTwo() {
+		System.out.println("Asif is a member of team TryHard.");
+	}
+	
+	/**
+	 * Post: Outputs a description of the third member
+	 * of team TryHard.
+	 */
+	public void personThree() {
+		System.out.println("Dhruv is a member of team TryHard.");
+	}
+	
+	/**
+	 * Post: Outputs a description of the fourth member 
+	 * of team TryHard
+	 */
+	public void personFour() {
+		System.out.println("Chinh is a member of team TryHard.");
+	}
+	
+}
---
+++ src/EasterMain.java
@@ -0,0 +1,20 @@
+/*
+ * Easter Egg Iteration
+ * Description: Runs the easter egg iteration.
+ */
+
+/**
+ * 
+ * @author Asif Jamal
+ * @author Dhruv Balakrishnan
+ * @author Chinh Nguyen
+ * @author Biirhanu Zerefa
+ */
+public class EasterMain {
+
+	public static void main(String[] args) {
+		EasterEgg egg = new EasterEgg();
+		egg.run();
+	}
+
+}
